0c5435a5bb0d7503f90a5bdc52afb2112ca57b24
evitalis <evitalis@users.noreply.github.com>
2017-02-27 17:58:12 -0600

    Initial form layout

--- cvechk/views.py
+++ cvechk/views.py
@@ -1,6 +1,14 @@
 from flask import render_template, flash, redirect, session, url_for, request
 
+from cvechk import app
+from cvechk.forms import CVEInputForm
+
 
 @app.route('/')
 def display_index():
-    return render_template('index.html')
+    return render_template('index.html', form=CVEInputForm())
+
+
+@app.route('/submit_check', methods=['POST'])
+def submit_check():
+    pass
