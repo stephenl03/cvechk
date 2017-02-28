f0caf9086bb74eb79e93d25f2db98c4b30938a26
evitalis <evitalis@users.noreply.github.com>
2017-02-27 18:13:13 -0600

    Add template results form

--- cvechk/forms.py
+++ cvechk/forms.py
@@ -13,3 +13,7 @@ class CVEInputForm(FlaskForm):
     uos = SelectField(coerce=str, choices=os_choices())
     uinputtext = TextAreaField()
     submit_button = SubmitField('Submit')
+
+
+class ResultsForm(FlaskForm):
+    back_button = SubmitField('Back')
