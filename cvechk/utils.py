505feaf10ad842087ab88549e7badd00b86151b2
evitalis <evitalis@users.noreply.github.com>
2017-03-02 18:48:27 -0600

    Begin redis integration

--- cvechk/utils.py
+++ cvechk/utils.py
@@ -1,5 +1,13 @@
+from cvechk import app
+
 import re
 import redis
+
+
+redis_host = app.config['REDISHOST']
+redis_port = app.config['REDISPORT']
+redis_pass = app.config['REDISPASS']
+redis_db = app.config['REDISDB']
 
 
 def get_cve_text(intext):
@@ -7,9 +15,18 @@ def get_cve_text(intext):
     return cve_pattern.findall(intext)
 
 
-def redis_get_data():
-    pass
+def redis_get_data(os, cvelist):
+    redis_conn = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+    cvedata = {}
+
+    for cve in cvelist:
+        cache = redis_conn.hgetall(os + ':' + cve)
+        cvedata[cve] = cache
+
+    return cvedata
 
 
-def redis_set_data():
-    pass
+def redis_set_data(os, cvedata):
+    redis_conn = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+    for cve in cvedata.keys():
+        redis_conn.hmset(os + ':' + cve, cvedata[cve])
