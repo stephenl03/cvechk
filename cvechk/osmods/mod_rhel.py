ceabd3babc4cd83c2dd2783c030222f1483093a5
evitalis <evitalis@users.noreply.github.com>
2017-03-04 10:09:46 -0600

    Add read from cache

--- cvechk/osmods/mod_rhel.py
+++ cvechk/osmods/mod_rhel.py
@@ -1,4 +1,9 @@
+from cvechk import app
+from cvechk.utils import redis_set_data
+
 import requests
+
+enable_cache = app.config['ENABLE_CACHE']
 
 
 def rh_get_data(cvenum):
@@ -15,7 +20,7 @@ def rh_get_data(cvenum):
     return r.json()
 
 
-def rh_get_pkgs(cve, os):
+def rh_get_pkgs(os, cve):
     os_list = {'rhel6': 'Red Hat Enterprise Linux 6',
                'rhel7': 'Red Hat Enterprise Linux 7'}
     cve_urls = []
@@ -25,18 +30,19 @@ def rh_get_pkgs(cve, os):
     cve_url = 'https://access.redhat.com/security/cve/'
     errata_url = 'https://rhn.redhat.com/errata/'
 
-    for c in cve:
-        try:
-            for i in rh_get_data(c)['affected_release']:
-                cve_urls.append(cve_url + c)
-                if i['product_name'] == os_list[os]:
-                    advisory = i['advisory'].replace(':', '-')
-                    rhsa_urls.append(errata_url + advisory + '.html')
-                    packages.append(i['package'])
-        except TypeError:
-            continue
+    cvedata = {}
 
-    # Use set() here to avoid duplicate output.
-    return dict(cveurls=sorted(set(cve_urls)),
-                rhsa=sorted(set(rhsa_urls)),
-                pkgs=sorted(set(packages)))
+    rhdata = rh_get_data(cve)['affected_release']
+    for i in rhdata:
+        cve_urls.append(cve_url + cve)
+        if i['product_name'] == os_list[os]:
+            advisory = i['advisory'].replace(':', '-')
+            rhsa_urls.append(errata_url + advisory + '.html')
+            packages.append(i['package'])
+
+            cvedata = dict(cveurls=sorted(set(cve_urls)),
+                           rhsa=sorted(set(rhsa_urls)),
+                           pkgs=sorted(set(packages)))
+    redis_set_data('{}:{}'.format(os, cve), cvedata)
+
+    return cvedata
