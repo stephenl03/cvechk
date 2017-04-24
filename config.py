f1924ce6db78e963f4876ba056c1187b0565fc78
evitalis <evitalis@users.noreply.github.com>
2017-04-24 17:43:47 -0500

    Add SERVER_NAME setting and comments

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+# SECRET_KEY is used for session information and form input. 
+# It should be set to something secure and random.
+#
+# SEVER_NAME should match the URL being handled by cvechk in order to allow
+# for subdomain usage.
 SECRET_KEY = 'changeme'
+SERVER_NAME = 'example.com'
 
+# REDISPASS is an optional password being used for the Redis instance.
 REDISDB = 0
 REDISHOST = '127.0.0.1'
 REDISPORT = 6379
