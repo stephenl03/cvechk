8eec58a90550478e8af9bc3ec2ac8e029d06b918
evitalis <evitalis@users.noreply.github.com>
2017-05-17 18:52:22 -0500

    Rewrite JSON checks

--- cvechk/osmods/mod_rhel.py
+++ cvechk/osmods/mod_rhel.py
@@ -21,6 +21,26 @@ def rh_api_data(cvenum):
 def rh_get_data(os, cve):
     """ Utilize Red Hat API to get specific data on provided CVE. """
 
+    rhdata = rh_api_data(cve)
+    cvedata = {}
+
+    if rhdata:
+        cvedata = check_affected_rel(rhdata, cve, os)
+    if not cvedata:
+        cvedata = check_release_data(rhdata, cve, os)
+    if not cvedata:
+        testurl = requests.get(f'https://access.redhat.com/security/cve/{cve}')
+        if testurl.status_code == 404:
+            cvedata = {'cveurl': f'https://cve.mitre.org/cgi-bin/cvename.cgi?name={cve}',  # noqa
+                       'state': 'Not found in Red Hat database'}
+        else:
+            cvedata = {'cveurl': f'https://access.redhat.com/security/cve/{cve}'} # noqa
+
+    redis_set_data('cvechk:{0}:{1}'.format(os, cve), cvedata)
+    return cvedata
+
+
+def check_affected_rel(rhdata, cve, os):
     os_list = {'EL_6': 'Red Hat Enterprise Linux 6',
                'EL_7': 'Red Hat Enterprise Linux 7'}
 
@@ -28,12 +48,6 @@ def rh_get_data(os, cve):
     errata_url = 'https://rhn.redhat.com/errata/'
 
     cvedata = {}
-
-    rhdata = rh_api_data(cve)
-
-    ''' Attempt to first get applicable packages, if not available then get
-        the Red Hat set state, including will not fix, otherwise skip the CVE.
-    '''
     try:
         for ar in rhdata['affected_release']:
             if ar['product_name'] == os_list[os]:
@@ -46,31 +60,23 @@ def rh_get_data(os, cve):
                                pkg=package)
                 cvedata['state'] = 'Affected'
                 break
+
+        return cvedata
     except KeyError:
-        rhellogger.warning(f'No affected_release data for {os} {cve}')
-        try:
-            for ar in rhdata['package_state']:
-                if ar['product_name'] == os_list[os]:
-                    cvedata = dict(cveurl=cve_url + cve)
-                    cvedata['state'] = ar['fix_state']
-                    break
-        except KeyError:
-            ''' If CVE is not found check for a valid URL anyway for additional
-                information. Provide alternative link and warning if URL is not
-                valid for Red Hat operating systems. '''
-            rhellogger.warning(f'No package_state data for {os} {cve}')
+        rhellogger.warning(f'No affected release found for {cve} ({os})')
+        return None
 
-            r = requests.get(f'https://access.redhat.com/security/cve/{cve}')
-            if r.status_code == 404:
-                cvedata = {'cveurl': f'https://cve.mitre.org/cgi-bin/cvename.cgi?name={cve}',  # noqa
-                           'state': 'Not found in Red Hat database'}
-            else:
-                cvedata = {'cveurl': f'https://access.redhat.com/security/cve/{cve}'}  # noqa
-        except:
-            rhellogger.exception('Uncaught exception has occurred')
-    except:
-        rhellogger.exception('Uncaught exception has occurred')
 
-    redis_set_data('cvechk:{0}:{1}'.format(os, cve), cvedata)
+def check_release_data(rhdata, cve, os):
+    os_list = {'EL_6': 'Red Hat Enterprise Linux 6',
+               'EL_7': 'Red Hat Enterprise Linux 7'}
+
+    cve_url = 'https://access.redhat.com/security/cve/'
+
+    cvedata = {}
+    for ar in rhdata['package_state']:
+        if ar['product_name'] == os_list[os]:
+            cvedata = dict(cveurl=cve_url + cve)
+            cvedata['state'] = ar['fix_state']
 
     return cvedata
