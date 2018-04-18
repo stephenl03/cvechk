80b666dd039bf92014d35d94e21c837930a978f9
evitalis <evitalis@users.noreply.github.com>
2018-04-18 18:53:11 -0500

    Remove broken API code

--- cvechk/views.py
+++ cvechk/views.py
@@ -16,11 +16,6 @@ def display_index():
     return render_template('index.html', form=CVEInputForm())
 
 
-@app.route('/api_info')
-def show_apiinfo():
-    return render_template('api_info.html')
-
-
 @app.route('/privacy')
 def display_privacy():
     return render_template('privacy.html')
@@ -53,36 +48,3 @@ def results():
     else:
         flash('No data entered, please enter some data in the field above.')
         return redirect(url_for('display_index'))
-
-
-@app.route('/', subdomain='api', methods=['GET'])
-def api_cvelist():
-    data = {}
-    cves = get_cve_text(request.args['cvelist'])
-    os = request.args['os']
-    oformat = request.args['format']
-
-    try:
-        data = redis_get_data(os, cves)
-    except:
-        viewlogger.exception('Unable to connect to Redis instance')
-
-    if not data:
-        data = mod_rhel.rh_get_data(os, cves)
-
-    if oformat in ['text', 'json']:
-        if oformat == 'text':
-            output = f'Selected OS: {os.replace("_", " ")}\n'
-            for cve in data:
-                output += f'CVE: {cve}\n'
-                output += f'CVE URL: {data[cve]["cveurl"]}\n'
-
-                if os.startswith('EL'):
-                    output += f'RHSA URL: {data[cve]["rhsaurl"]}\n'
-
-                output += f'Fixed Packages: {data[cve]["pkg"]}\n\n'
-                return output
-        else:
-            response = make_response(jsonify(data))
-            response.headers['Content-Type'] = 'application/json'
-            return response
